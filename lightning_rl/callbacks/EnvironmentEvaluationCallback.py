bc829f3be0800655b28b87085c5b219d907d25d3
Jordi <jordismit20000@gmail.com>
2021-04-02 15:49:12 +0200

    on_train_end

--- lightning_rl/callbacks/EnvironmentEvaluationCallback.py
+++ lightning_rl/callbacks/EnvironmentEvaluationCallback.py
@@ -107,7 +107,7 @@ class EnvironmentEvaluationCallback(Callback):
 
         return list(lengths), list(returns)
 
-    def on_fit_end(self, trainer, pl_module: LightningModule) -> None:
+    def on_train_end(self, trainer, pl_module: LightningModule) -> None:
         if self.n_test_episodes <= 0:
             return
 
